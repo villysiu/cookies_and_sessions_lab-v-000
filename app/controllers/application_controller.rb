fdcd47a0a56721f402819b864ea51c7b75b16334
Villy Siu <villysiu@gmail.com>
2017-11-09 23:35:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   #protect_from_forgery with: :exception
   helper_method :current_cart
 
-  def current_cart
+  def cart
     session[:cart] ||= []
   end
 end
